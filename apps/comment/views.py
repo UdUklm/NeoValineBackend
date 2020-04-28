a0d104b4a2d7207e05a3ca4c1816031866f82998
UdUklm <1491989333@qq.com>
2020-04-28 15:28:36 +0800

    增加分页功能

--- apps/comment/views.py
+++ apps/comment/views.py
@@ -1,10 +1,21 @@
 from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework import filters
+from rest_framework import pagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 from .models import CommentModel
 from .serializers import CommentSerializer
+
+
+class CommentPagination(pagination.PageNumberPagination):
+    page_size_query_param = 'page_size'  # 每页的大小
+    page_query_param = 'page'  # 要的是哪一页
+    max_page_size = 1000  # 分页上限
+
+    def get_paginated_response(self, data):
+        return Response(data)
 
 
 class CommentViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
@@ -14,9 +25,10 @@ class CommentViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
     create: 添加评论
     list: 获取评论列表
     detail: 评论详情
-        """
+    """
     queryset = CommentModel.objects.filter(display=True)  # 查询集过滤
     serializer_class = CommentSerializer
+    pagination_class = CommentPagination
     filter_backends = (
         DjangoFilterBackend,
         filters.OrderingFilter
