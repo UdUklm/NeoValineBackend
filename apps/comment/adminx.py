94037357cf19eef94cbc95a3601c83b8ca9d2208
UdUklm <1491989333@qq.com>
2020-05-03 23:49:16 +0800

    重新实现了邮件提醒功能

--- apps/comment/adminx.py
+++ apps/comment/adminx.py
@@ -1,5 +1,9 @@
-from .models import CommentModel
 import xadmin
+
+from .models import CommentModel
+from apps.utils.mail import mail_display_notice
+from apps.utils.mail import mail_reply_notice
+from NeoValineBackend.hidden_options import SITE_INFO
 
 
 class CommentModelAdmin(object):
@@ -13,6 +17,43 @@ class CommentModelAdmin(object):
     search_fields = ['id', 'url', 'comment', 'nick', 'mail', 'link', 'rid',
                      'pid', 'ip', 'ua']
     readonly_fields = ['id', 'url', 'rid', 'pid', 'ip', 'ua']
+    list_editable = ['display']
+
+    def save_models(self):
+        """
+        检查是否有新的通知需要发邮件，包括：
+        1. 过审的评论通知
+        2. 新回复的通知
+        """
+        obj = self.new_obj
+        obj.save()
+        site_url = SITE_INFO['SITE_URL']
+
+        if obj.display and not obj.notified:
+            # 审核通过，但没有通知过
+            if obj.mail:
+                # 填写了邮箱
+                display_notice_ret = \
+                    mail_display_notice(post_url=site_url+obj.url,
+                                        receiver=obj.mail,
+                                        comment=obj.comment)
+                if not display_notice_ret:
+                    # 邮件未发送成功
+                    return
+            if obj.pid:
+                # 该评论是条回复
+                pre = CommentModel.objects.get(id=obj.pid, mail__isnull=False)
+                reply_notice_ret = \
+                    mail_reply_notice(post_url=site_url+pre.url,
+                                      receiver=pre.mail,
+                                      nick=pre.nick,
+                                      comment=pre.comment,
+                                      reply_nick=obj.nick,
+                                      reply_comment=obj.comment)
+                if not reply_notice_ret:
+                    return
+            obj.notified = True
+            obj.save()
 
 
 xadmin.site.register(CommentModel, CommentModelAdmin)
